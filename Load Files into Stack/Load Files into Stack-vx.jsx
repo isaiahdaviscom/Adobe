539995a34ac3b28ef86b4738ca12acc60788c123
Isaiah Davis <idavis@igainc.com>
2020-09-10 14:10:44 -0500

    updates

--- Load Files into Stack/Load Files into Stack-vx.jsx
+++ Load Files into Stack/Load Files into Stack-vx.jsx
@@ -20,41 +20,22 @@
 // END__HARVEST_EXCEPTION_ZSTRING
 
 */
-
-// debug level: 0-2 (0:disable, 1:break on error, 2:break at beginning)
-//$.level = (Window.version.search("d") != -1) ? 1 : 0;	// This chokes bridge
 $.level = 0;
-
-// debugger; // launch debugger on next line
-
-// on localized builds we pull the $$$/Strings from a .dat file
 $.localize = true;
-
-// Put header files in a "Stack Scripts Only" folder.  The "...Only" tells
-// PS not to place it in the menu.  For that reason, we do -not- localize that
-// portion of the folder name.
 var g_StackScriptFolderPath = app.path + "/" + localize("$$$/ScriptingSupport/InstalledScripts=Presets/Scripts") + "/"
   + localize("$$$/private/LoadStack/StackScriptOnly=Stack Scripts Only/");
-
 $.evalFile(g_StackScriptFolderPath + "LatteUI.jsx");
-
 $.evalFile(g_StackScriptFolderPath + "StackSupport.jsx");
-
 $.evalFile(g_StackScriptFolderPath + "CreateImageStack.jsx");
-
-/************************************************************/
-// loadLayers routines
 
 loadLayers = new ImageStackCreator(localize("$$$/AdobePlugin/Shared/LoadStack/Process/Name=Load Layers"),
   localize('$$$/AdobePlugin/Shared/LoadStack/Auto/untitled=Untitled'));
 
-// LoadLayers is less restrictive than MergeToHDR
 loadLayers.mustBeSameSize = false;	// Images' height & width don't need to match
 loadLayers.mustBeUnmodifiedRaw = false;	// Exposure adjustements in Camera raw are allowed
 loadLayers.mustNotBe32Bit = false;	// 32 bit images
 loadLayers.createSmartObject = false;	// If true, option to create smart object is checked.
 
-// Add hooks to read the value of the "Create Smart Object" checkbox
 loadLayers.customDialogSetup = function (w) {
   w.findControl('_createSO').value = loadLayers.createSmartObject;
   if (!app.featureEnabled(localize("$$$/private/ExtendedImageStackCreation=ImageStack Creation")))
@@ -65,7 +46,6 @@ loadLayers.customDialogFunction = function (w) {
   loadLayers.createSmartObject = w.findControl('_createSO').value;
 }
 
-// Override the default to use "Auto" alignment.
 loadLayers.alignStack = function (stackDoc) {
   selectAllLayers(stackDoc, 2);
   alignLayersByContent("Auto");
@@ -78,17 +58,14 @@ loadLayers.stackLayers = function () {
   if (!stackDoc)
     return;
 
-  // Nuke the "destination" layer that got created (M2HDR holdover)
   stackDoc.layers[this.pluginName].remove();
 
-  // Stack 'em up.
   if (this.createSmartObject) {
     selectAllLayers(stackDoc);
     executeAction(knewPlacedLayerStr, new ActionDescriptor(), DialogModes.NO);
   }
 }
 
-// "Main" execution of Merge to HDR
 loadLayers.doInteractiveLoad = function () {
   this.getFilesFromBridgeOrDialog(localize("$$$/private/LoadStack/LoadLayersexv=LoadLayers.exv"));
 
@@ -113,9 +90,7 @@ loadLayers.intoStack = function (filelist, alignFlag) {
     this.stackElements.push(new StackElement((typeof (f) == 'string') ? File(f) : f));
   }
 
-  if (this.stackElements.length > 1)
-    this.stackLayers();
+  if (this.stackElements.length > 1) this.stackLayers();
 }
 
-if (typeof (loadLayersFromScript) == 'undefined')
-  loadLayers.doInteractiveLoad();
+if (typeof (loadLayersFromScript) == 'undefined') loadLayers.doInteractiveLoad();

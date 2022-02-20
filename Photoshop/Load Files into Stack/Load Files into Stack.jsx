30b0aaeaf299eddc0217a347f57ac98508901d7e
Isaiah Davis <isaiah.davisx@gmail.com>
2022-02-20 13:02:02 -0600

    updates

--- Photoshop/Load Files into Stack/Load Files into Stack.jsx
+++ Photoshop/Load Files into Stack/Load Files into Stack.jsx
@@ -22,7 +22,7 @@
 */
 
 // debug level: 0-2 (0:disable, 1:break on error, 2:break at beginning)
-//$.level = (Window.version.search("d") != -1) ? 1 : 0;	// This chokes bridge
+//$.level = (Window.version.search("d") != -1) ? 1 : 0; // This chokes bridge
 $.level = 0;
 
 // debugger; // launch debugger on next line
@@ -33,8 +33,8 @@ $.localize = true;
 // Put header files in a "Stack Scripts Only" folder.  The "...Only" tells
 // PS not to place it in the menu.  For that reason, we do -not- localize that
 // portion of the folder name.
-var g_StackScriptFolderPath = app.path + "/" + localize("$$$/ScriptingSupport/InstalledScripts=Presets/Scripts") + "/"
-  + localize("$$$/private/LoadStack/StackScriptOnly=Stack Scripts Only/");
+var g_StackScriptFolderPath = app.path + "/"+ localize("$$$/ScriptingSupport/InstalledScripts=Presets/Scripts") + "/"
+                                        + localize("$$$/private/LoadStack/StackScriptOnly=Stack Scripts Only/");
 
 $.evalFile(g_StackScriptFolderPath + "LatteUI.jsx");
 
@@ -45,77 +45,87 @@ $.evalFile(g_StackScriptFolderPath + "CreateImageStack.jsx");
 /************************************************************/
 // loadLayers routines
 
-loadLayers = new ImageStackCreator(localize("$$$/AdobePlugin/Shared/LoadStack/Process/Name=Load Layers"),
-  localize('$$$/AdobePlugin/Shared/LoadStack/Auto/untitled=Untitled'));
+loadLayers = new ImageStackCreator( localize("$$$/AdobePlugin/Shared/LoadStack/Process/Name=Load Layers"),
+                                          localize('$$$/AdobePlugin/Shared/LoadStack/Auto/untitled=Untitled' ) );
 
 // LoadLayers is less restrictive than MergeToHDR
-loadLayers.mustBeSameSize = false;	// Images' height & width don't need to match
-loadLayers.mustBeUnmodifiedRaw = false;	// Exposure adjustements in Camera raw are allowed
-loadLayers.mustNotBe32Bit = false;	// 32 bit images
-loadLayers.createSmartObject = false;	// If true, option to create smart object is checked.
+loadLayers.mustBeSameSize           = false;    // Images' height & width don't need to match
+loadLayers.mustBeUnmodifiedRaw      = false;    // Exposure adjustements in Camera raw are allowed
+loadLayers.mustNotBe32Bit           = false;    // 32 bit images
+loadLayers.createSmartObject        = false;    // If true, option to create smart object is checked.
 
 // Add hooks to read the value of the "Create Smart Object" checkbox
-loadLayers.customDialogSetup = function (w) {
-  w.findControl('_createSO').value = loadLayers.createSmartObject;
-  if (!app.featureEnabled(localize("$$$/private/ExtendedImageStackCreation=ImageStack Creation")))
-    w.findControl('_createSO').hide();
+loadLayers.customDialogSetup = function( w )
+{
+    w.findControl('_createSO').value = loadLayers.createSmartObject;
+    if (! app.featureEnabled( localize( "$$$/private/ExtendedImageStackCreation=ImageStack Creation" ) ))
+        w.findControl('_createSO').hide();
 }
 
-loadLayers.customDialogFunction = function (w) {
-  loadLayers.createSmartObject = w.findControl('_createSO').value;
+loadLayers.customDialogFunction = function( w )
+{
+    loadLayers.createSmartObject = w.findControl('_createSO').value;
 }
 
 // Override the default to use "Auto" alignment.
-loadLayers.alignStack = function (stackDoc) {
-  selectAllLayers(stackDoc, 2);
-  alignLayersByContent("Auto");
+loadLayers.alignStack = function( stackDoc )
+{
+    selectAllLayers(stackDoc, 2);
+    alignLayersByContent( "Auto" );
 }
 
-loadLayers.stackLayers = function () {
-  var result, i, stackDoc = null;
-
-  stackDoc = this.loadStackLayers();
-  if (!stackDoc)
-    return;
-
-  // Nuke the "destination" layer that got created (M2HDR holdover)
-  stackDoc.layers[this.pluginName].remove();
-
-  // Stack 'em up.
-  if (this.createSmartObject) {
-    selectAllLayers(stackDoc);
-    executeAction(knewPlacedLayerStr, new ActionDescriptor(), DialogModes.NO);
-  }
+loadLayers.stackLayers = function()
+{
+    var result, i, stackDoc = null;
+    
+    stackDoc = this.loadStackLayers();
+    if (! stackDoc)
+        return;
+    
+    // Nuke the "destination" layer that got created (M2HDR holdover)
+    stackDoc.layers[this.pluginName].remove();
+    
+    // Stack 'em up.
+    if (this.createSmartObject)
+    {
+        console.log
+        selectAllLayers( stackDoc );
+        executeAction( knewPlacedLayerStr, new ActionDescriptor(), DialogModes.NO );
+    }
 }
 
 // "Main" execution of Merge to HDR
-loadLayers.doInteractiveLoad = function () {
-  this.getFilesFromBridgeOrDialog(localize("$$$/private/LoadStack/LoadLayersexv=LoadLayers.exv"));
+loadLayers.doInteractiveLoad = function ()
+{
+    this.getFilesFromBridgeOrDialog( localize("$$$/private/LoadStack/LoadLayersexv=LoadLayers.exv") );
 
-  if (this.stackElements)
-    this.stackLayers();
-  else
-    return 'cancel';
+    if (this.stackElements)
+        this.stackLayers();
+    else
+        return 'cancel';
 }
 
-loadLayers.intoStack = function (filelist, alignFlag) {
-  if (typeof (alignFlag) == 'boolean')
-    loadLayers.useAlignment = alignFlag;
-
-  if (filelist.length < 2) {
-    alert(localize("$$$/AdobeScripts/Shared/LoadLayers/AtLeast2=At least two files must be selected to create a stack."), this.pluginName, true);
-    return;
-  }
-  var j;
-  this.stackElements = new Array();
-  for (j in filelist) {
-    var f = filelist[j];
-    this.stackElements.push(new StackElement((typeof (f) == 'string') ? File(f) : f));
-  }
-
-  if (this.stackElements.length > 1)
-    this.stackLayers();
+loadLayers.intoStack = function(filelist, alignFlag)
+{
+    if (typeof(alignFlag) == 'boolean')
+        loadLayers.useAlignment = alignFlag;
+        
+    if (filelist.length < 2)
+    {
+        alert(localize("$$$/AdobeScripts/Shared/LoadLayers/AtLeast2=At least two files must be selected to create a stack."), this.pluginName, true );
+        return;
+    }
+    var j;
+    this.stackElements = new Array();
+    for (j in filelist)
+    {
+        var f = filelist[j];
+        this.stackElements.push( new StackElement( (typeof(f) == 'string') ? File(f) : f ) );
+    }
+        
+    if (this.stackElements.length > 1)
+        this.stackLayers();
 }
 
-if (typeof (loadLayersFromScript) == 'undefined')
-  loadLayers.doInteractiveLoad();
+if (typeof(loadLayersFromScript) == 'undefined')
+    loadLayers.doInteractiveLoad();
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 ### XD Plugin Development
 [Quick Start](https://adobexdplatform.com/plugin-docs/tutorials/quick-start/)
+
+### Projects
+[Font Awesome Glyph Panel](https://fontawesome.com/icons?d=gallery&q=recorder&m=free)
+url param breakdown and order
+(d) display
+	gallery
+	listing
+(q) query - urlencoded
+	`term`
+(s) style - comma separated
+	solid
+	regular
+	light
+	duotone
+	brands
+(v) version
+	`5.15.1
+(c) - comma separated
+	accessibility
+	alert
+	animals
+	... (73)
+(m)
+	free
+	pro
